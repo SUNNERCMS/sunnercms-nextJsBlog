af607fc20129c5186e6e9e7df3dbe026749eaae9
zhaoxiang.sun <sunnercms@163.com>
2020-10-28 11:40:36 +0800

    add: abloutlink模块animation

--- components/AboutLink/index.js
+++ components/AboutLink/index.js
@@ -1,19 +1,20 @@
 /*
  * @Author: your name
  * @Date: 2020-10-25 22:33:24
- * @LastEditTime: 2020-10-26 23:42:50
+ * @LastEditTime: 2020-10-28 11:38:55
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /sunnercms-nextJsBlog/components/AboutLink/index.js
  */
 import React, { Component } from 'react';
 import styles from './index.module.less';
+import animations from '../../styles/animation.less'
 class AboutLink extends Component {
   render() {
     return (
       <div className={styles.aboutLinkContainer}>
-        <div className={styles.about}>关于</div>
-        <div className={styles.link}>+友情链接</div>
+        <div className={animations.hvrBounceToRight}>关于</div>
+        <div className={animations.hvrBounceToLeft}>+友情链接</div>
       </div>
     );
   }
