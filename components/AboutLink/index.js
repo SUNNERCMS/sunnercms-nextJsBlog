4a93015c00c91fbde345af33092362fadef3b54f
zhaoxiang.sun <sunnercms@163.com>
2020-10-30 23:49:35 +0800

    fix: 右侧导航弹层头像移动端自适应

--- components/AboutLink/index.js
+++ components/AboutLink/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-10-25 22:33:24
- * @LastEditTime: 2020-10-28 11:38:55
+ * @LastEditTime: 2020-10-30 23:38:48
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /sunnercms-nextJsBlog/components/AboutLink/index.js
@@ -9,15 +9,15 @@
 import React, { Component } from 'react';
 import styles from './index.module.less';
 import animations from '../../styles/animation.less'
-class AboutLink extends Component {
-  render() {
-    return (
-      <div className={styles.aboutLinkContainer}>
-        <div className={animations.hvrBounceToRight}>关于</div>
-        <div className={animations.hvrBounceToLeft}>+友情链接</div>
-      </div>
-    );
-  }
-}
 
-export default AboutLink;
+export default function AboutLink() {
+  const aboutMeLink = () => {
+    window.open("https://sunzhaoxiang.blog.csdn.net");
+  }
+  return (
+    <div className={styles.aboutLinkContainer}>
+      <div className={animations.hvrBounceToRight}>关于</div>
+      <div className={animations.hvrBounceToLeft} onClick={aboutMeLink}>+友情链接</div>
+    </div>
+  )
+}
