bbdef46fb9ecee14d422132d1bcd49320e0d005a
zhaoxiang.sun <sunnercms@163.com>
2020-10-30 22:57:36 +0800

    add: 响应式布局

--- components/HotPages/index.js
+++ components/HotPages/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-10-24 21:55:20
- * @LastEditTime: 2020-10-27 22:53:56
+ * @LastEditTime: 2020-10-30 22:56:32
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /sunnercms-nextJsBlog/components/HotPages/index.js
@@ -48,6 +48,28 @@ class HotPages extends Component {
                 </div>
               </CopyToClipboard>
               <img
+                src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3155886573,2573739945&fm=26&gp=0.jpg"
+                className={styles.itemImage}
+              />
+            </div>
+            <div className={styles.itemInfo}>
+              <div className={styles.itemName}>新增qq应用</div>
+              <div className={styles.itemDate}>2019年12月24日</div>
+              <p className={styles.itemDesc}>
+                激发了房间里的卷发简法定放假啊老骥伏枥的减肥 u 让卷发短款礼服发发呆了卷发短款两间房单就发儿啊就发来的附加费阿迪风景
+              </p>
+              <div className={styles.readMore}>阅读更多</div>
+            </div>
+          </div>
+
+          <div className={styles.contentItem}>
+            <div className={styles.imgContainer}>
+              <CopyToClipboard text={this.state.value} onCopy={this.onCopyCallBack}>
+                <div className={ `${styles.imgMask} ${styles.sweepToBottom}`} onClick={this.copyLinkHandle}>
+                  <i className="iconfont icon-fuzhilianjie"></i>
+                </div>
+              </CopyToClipboard>
+              <img
                 src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603651196522&di=a16700caf33d74f13f72dc94aeb6bdaf&imgtype=0&src=http%3A%2F%2Fimg.yuntiy.com%2Fillustration%2F1535098791991.jpg"
                 className={styles.itemImage}
               />
@@ -61,24 +83,15 @@ class HotPages extends Component {
               <div className={styles.readMore}>阅读更多</div>
             </div>
           </div>
+
+
           <div className={styles.contentItem}>
-          <div className={styles.imgContainer}>
-              <img
-                src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3155886573,2573739945&fm=26&gp=0.jpg"
-                className={styles.itemImage}
-              />
-            </div>
-            <div className={styles.itemInfo}>
-              <div className={styles.itemName}>新增qq应用</div>
-              <div className={styles.itemDate}>2019年12月24日</div>
-              <p className={styles.itemDesc}>
-                激发了房间里的卷发简单就发儿啊就发来的附加费阿迪风景
-              </p>
-              <div className={styles.readMore}>阅读更多</div>
-            </div>
-          </div>
-          <div className={styles.contentItem}>
-          <div className={styles.imgContainer}>
+            <div className={styles.imgContainer}>
+              <CopyToClipboard text={this.state.value} onCopy={this.onCopyCallBack}>
+                <div className={ `${styles.imgMask} ${styles.sweepToBottom}`} onClick={this.copyLinkHandle}>
+                  <i className="iconfont icon-fuzhilianjie"></i>
+                </div>
+              </CopyToClipboard>
               <img
                 src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=389917905,3016311303&fm=26&gp=0.jpg"
                 className={styles.itemImage}
@@ -88,7 +101,7 @@ class HotPages extends Component {
               <div className={styles.itemName}>新增qq应用</div>
               <div className={styles.itemDate}>2019年12月24日</div>
               <p className={styles.itemDesc}>
-                激发了房间里的卷发简单就发儿啊就发来的附加费阿迪风景
+                激发了房间里的卷发简法定放假啊老骥伏枥的减肥 u 让卷发短款礼服发发呆了卷发短款两间房单就发儿啊就发来的附加费阿迪风景
               </p>
               <div className={styles.readMore}>阅读更多</div>
             </div>
